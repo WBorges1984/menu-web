0bd8f06cd5fc42aecfcc41ea22b1f4dbf8d75208
Willian Borges <134416210+WBorges1984@users.noreply.github.com>
2025-05-27 19:37:21 -0300

    header feito

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,45 @@
-
+import { useState } from "react";
 import ButtonMenu from "../buttonMenu/ButtonMenu";
 import "./header.style.css";
 
+const MENU_ITEMS = {
+  INICIO: 'inicio',
+  PROMOCOES: 'promocoes',
+  PEDIDOS: 'pedidos',
+  ENTRAR_CADASTRAR: 'entcad',
+};
+
 export const Header = () => {
+
+  const [activeItem, setActiveItem] = useState(MENU_ITEMS.INICIO);
+
+  const handleMenuClick = (menuName) => {
+    setActiveItem(menuName);
+  };
+
   return (
     <div className="containerHeader">
       <header className="headerMenu">
-        <nav>
-          <ButtonMenu name={'Início'} />
-          <button>Promoções</button>
-          <button>Pedidos</button>
-          <button>Entrar/Cadastrar</button>
-        </nav>
+        <ButtonMenu
+          name={"Início"}
+          active={activeItem === MENU_ITEMS.INICIO}
+          onclick={() => handleMenuClick(MENU_ITEMS.INICIO)}
+        />
+        <ButtonMenu
+          name={"Promoções"}
+          active={activeItem === MENU_ITEMS.PROMOCOES}
+          onclick={() => handleMenuClick(MENU_ITEMS.PROMOCOES)}
+        />
+        <ButtonMenu
+          name={"Pedidos"}
+          active={activeItem === MENU_ITEMS.PEDIDOS}
+          onclick={() => handleMenuClick(MENU_ITEMS.PEDIDOS)}
+        />
+        <ButtonMenu
+          name={"Entrar/Cadastrar"}
+          active={activeItem === MENU_ITEMS.ENTRAR_CADASTRAR}
+          onclick={() => handleMenuClick(MENU_ITEMS.ENTRAR_CADASTRAR)}
+        />
       </header>
     </div>
   );
--- src/components/buttonMenu/ButtonMenu.jsx
+++ src/components/buttonMenu/ButtonMenu.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react';
 import './buttonMenu.style.css'
 import { FaHome } from "react-icons/fa";
 
-export default function ButtonMenu({name}) {
+export default function ButtonMenu({name, active, onclick}) {
+
   return (
-    <div className='containerButtonMenu'>
-        <FaHome size={48}/>
-        <button className='btn btn-light'>{name}</button>
+    <div onClick={onclick} style={{backgroundColor: active ? 'white' : 'black', color: active ? 'black' : 'white'  }} className='containerButtonMenu'>
+        <FaHome size={28}/>
+        <button className='btnMenu'>{name}</button>
     </div>
   )
 }
